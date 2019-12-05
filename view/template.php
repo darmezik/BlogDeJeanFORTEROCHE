f9288bda9e9b9b6107de007773d95f843eb52257
darmezik <killiandarmezin@gmail.com>
2019-12-05 14:54:29 +0100

    add picture, css & modif template viewHome

--- view/template.php
+++ view/template.php
@@ -3,9 +3,26 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="css/style.css" rel="stylesheet" />
+        <link href="../public/css/style.css" rel="stylesheet" />
     </head>
     <body>
+        <header>
+            <div>
+                <img src="../public/images/photoJean.jpg" alt="Photo de Jean FORTEROCHE" />
+            </div>
+            <div>
+                <h1>Blog de Jean FORTEROCHE</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Livre</a></li>
+                    <li><a href="#">Bibliographie</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Connexion</a></li>
+                </ul>
+            </nav>
+        </header>
         <?= $content ?>
     </body>
 </html>
--- view/viewHome.php
+++ view/viewHome.php
@@ -2,4 +2,4 @@
 <?php ob_start(); ?>
 
 <?php $content = ob_get_clean(); ?>
-<?php require('view/template.php'); ?>
+<?php require('template.php'); ?>
