e7a26377e865d990d0529c9e6cf8dc09dddc0e50
darmezik <killiandarmezin@gmail.com>
2019-12-20 13:24:34 +0100

    modif css

--- index.php
+++ index.php
@@ -4,59 +4,66 @@ require('controller/BackendController.php');
 $GLOBALS["app_url"] = "http://127.0.0.1/BlogDeJeanForteroche/";
 $FrontendController = new FrontendController;
 $BackendController = new BackendController;
-switch($_GET['action'])
+if (isset($_GET['action']))
 {
-    case 'home':
-        $FrontendController->printHome();
-    break;
-    case 'book':
-        $FrontendController->printBook();
-    break;
-    case 'comments':
-        $FrontendController->printComments();
-    break;
-    case 'addComment':
-        $FrontendController->addComment();
-    break;
-    case 'reportComment':
-        $FrontendController->reportComment();
-    break;
-    case 'contact':
-        $FrontendController->printContact();
-    break;
-    case 'connect':
-        $FrontendController->printConnect();
-    break;
-    case 'testConnect':
-        $FrontendController->testConnect();
-    break;
-    case 'deconnect':
-        $BackendController->deconnect();
-    break;
-    case 'dashboard':
-        $BackendController->printDashboard();
-    break;
-    case 'commentsUp':
-        $BackendController->printCommentsUp();
-    break;
-    case 'deleteComment':
-        $BackendController->deleteComment();
-    break;
-    case 'addPost':
-        $BackendController->addPost();
-    break;
-    case 'deletePost':
-        $BackendController->deletePost();
-    break;
-    case 'postUp':
-        $BackendController->printPostUp();
-    break;
-    case 'modifPost':
-        $BackendController->modifPost();
-    break;
-    case 'delReport':
-        $BackendController->delReportComment();
-    break;
-    default:
-        $FrontendController->printHome();
+    switch($_GET['action'])
+    {
+        case 'home':
+            $FrontendController->printHome();
+        break;
+        case 'book':
+            $FrontendController->printBook();
+        break;
+        case 'comments':
+            $FrontendController->printComments();
+        break;
+        case 'addComment':
+            $FrontendController->addComment();
+        break;
+        case 'reportComment':
+            $FrontendController->reportComment();
+        break;
+        case 'contact':
+            $FrontendController->printContact();
+        break;
+        case 'connect':
+            $FrontendController->printConnect();
+        break;
+        case 'testConnect':
+            $FrontendController->testConnect();
+        break;
+        case 'deconnect':
+            $BackendController->deconnect();
+        break;
+        case 'dashboard':
+            $BackendController->printDashboard();
+        break;
+        case 'commentsUp':
+            $BackendController->printCommentsUp();
+        break;
+        case 'deleteComment':
+            $BackendController->deleteComment();
+        break;
+        case 'addPost':
+            $BackendController->addPost();
+        break;
+        case 'deletePost':
+            $BackendController->deletePost();
+        break;
+        case 'postUp':
+            $BackendController->printPostUp();
+        break;
+        case 'modifPost':
+            $BackendController->modifPost();
+        break;
+        case 'delReport':
+            $BackendController->delReportComment();
+        break;
+        default:
+            $FrontendController->printHome();
+    }
+}
+else
+{
+    $FrontendController->printHome();
 }
